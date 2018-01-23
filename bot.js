e4828e37f1c89f4a3548503a4e902e4265970f0e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-22 21:23:37 -0600

    new

--- bot.js
+++ bot.js
@@ -7,20 +7,34 @@ const prompt = require('prompt');
 var [,, arg, ...args] = process.argv;
 switch (arg) {
   case "new":
-  prompt.start()
-  prompt.get(['name', 'token'], (err, result) => {
-    if (result) {
-      var {name, token} = result
+    var arr = [],
+    name, token;
+    if (args[0]) {
+      name = args[0]
+    } else {
+      arr.push('name')
     }
-    if (!err) {
-      var path = `bots/${name}.js`
-      try {
-        fs.statSync(path);
-        console.log("This already exists")
-      } catch (err) {
-        if (err.code == "ENOENT") {
-          jsonfile.write.sync("tokens.json", Object.assign(jsonfile.load.sync("tokens.json"), {[name]:token}));
-          fs.writeFileSync(path,
+    if (args[1]) {
+      token = args[1]
+    } else {
+      arr.push('token')
+    }
+    prompt.start()
+    prompt.get(arr, (err, result) => {
+      if (result) {
+        name = name || result.name;
+        token = token || result.token;
+      }
+
+      if (!err) {
+        var path = `bots/${name}.js`
+        try {
+          fs.statSync(path);
+          console.log("This already exists")
+        } catch (err) {
+          if (err.code == "ENOENT") {
+            jsonfile.write.sync("tokens.json", Object.assign(jsonfile.load.sync("tokens.json"), {[name]:token}));
+            fs.writeFileSync(path,
 `const bot = new Bot(tokens.${name}, {
   message: message => {
     if (message.content == "hey") {
@@ -28,9 +42,9 @@ switch (arg) {
     }
   }
 })`)
+            }
           }
         }
-      }
-    })
-    break;
+      })
+      break;
 }
