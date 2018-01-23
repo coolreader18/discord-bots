8bc952ba3919e948910cdedf6087c1b0d32de80e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-22 21:09:51 -0600

    can get non-amp version of previous messages keeps going until it finds one

--- bots/amp.js
+++ bots/amp.js
@@ -1,13 +1,29 @@
 const got = require('got');
 const nonAmp = require('non-amp-site');
+const validUrl = require('valid-url');
 
 const bot = new Bot(tokens.amp, {
   message: async ({content, channel}) => {
+    var nonamp = async url => channel.send(`non-amp version: ${await nonAmp(url)}`)
     if (content.startsWith('!amp')) {
       if (content.match(/^!amp .+/)) {
-        channel.send(`non-amp version: ${await nonAmp(content.replace(/!amp /, ""))}`)
+        try {
+          nonamp(content.replace(/!amp /, ""));
+        } catch (e) {
+          if (e.message == "Document is not an AMP Page") {
+            channel.send("That's not an amp page")
+          }
+        }
       } else {
-        channel.send("indeed, my good sir :tophat: :champagne_glass:");
+        let msgs = channel.messages.array();
+        loop1:
+        for (var i = 2;;i++) {
+          let uri = validUrl.isWebUri(msgs[msgs.length - i].content);
+          if (uri) {
+            nonamp(uri)
+            break loop1;
+          }
+        }
       }
     }
   }
